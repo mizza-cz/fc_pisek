407e0993f6a5938d84b805aa05edbaf3802757d7
mizza-cz <mizza.cz666@gmail.com>
2021-02-17 15:13:49 +0100

    HP page

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
   $('.menu__btn').on('click', function () {
-    $('.menu').slideToggle();
+    $('.header__row').slideToggle();
   });
   $('.nav__link').on('click', function () {
     $(this).closest('.nav__list').toggleClass('active');
