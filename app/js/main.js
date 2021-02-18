1a17f85d9249f45647dec7fd707e140d3a194b70
mizza-cz <mizza.cz666@gmail.com>
2021-02-18 10:53:20 +0100

    add zapas and oparava clanek page

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,12 @@ $(function () {
   $('.nav__link').on('click', function () {
     $(this).closest('.nav__list').toggleClass('active');
   });
+  // gallery
+  $('.gallery__inner').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true,
+    },
+  });
 });
