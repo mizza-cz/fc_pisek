31f8d3ff92fac5d262b9cbe1171465e3ae747d5b
mizza-cz <mizza.cz666@gmail.com>
2021-03-23 16:10:57 +0100

    add archiv

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function () {
+  $('select').styler();
+  //presmerovani pomoci selectu
+  $('.select__redirect').change(function () {
+    location.href = $(this).val();
+  });
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
   $('.menu__btn').on('click', function () {
     $('.header__row').slideToggle();
   });
--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,7 @@ gulp.task('style', function () {
     .src([
       'node_modules/normalize.css/normalize.css',
       'node_modules/slick-carousel/slick/slick.css',
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
       'node_modules/magnific-popup/dist/magnific-popup.css',
     ])
     .pipe(concat('libs.min.css'))
@@ -38,6 +39,7 @@ gulp.task('script', function () {
     .src([
       'node_modules/mixitup/dist/mixitup.js',
       'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
       'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
     ])
     .pipe(concat('libs.min.js'))
